a5be31fcd6d36b026aa4e5cbbe544b79f8b63c4c
Rolland306 <rollandmuzeya@gmail.com>
2024-06-19 23:58:11 -0400

    Implemented ProductService. Automated Tests are next.

--- product-service/src/main/java/com/mukomana/product_service/service/ProductService.java
+++ product-service/src/main/java/com/mukomana/product_service/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.mukomana.product_service.service;
 
 import com.mukomana.product_service.dto.ProductRequest;
+import com.mukomana.product_service.dto.ProductResponse;
 import com.mukomana.product_service.model.Product;
 import com.mukomana.product_service.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +27,20 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
